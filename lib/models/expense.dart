2bd16da700679816aca19d93fe48b8b046a74d73
LbStoyanov <lbstoyanov1@gmail.com>
2023-06-30 19:08:02 +0300

    expense_tracker

---
+++ lib/models/expense.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+import 'package:uuid/uuid.dart';
+import 'package:intl/intl.dart';
+
+final dateFormater = DateFormat.yMd();
+
+const uuid = Uuid();
+
+enum Category { food, travel, leisure, work }
+
+const categoryIcons = {
+  Category.food: Icons.lunch_dining_rounded,
+  Category.travel: Icons.flight_outlined,
+  Category.leisure: Icons.movie_filter_outlined,
+  Category.work: Icons.work_rounded
+};
+
+class Expense {
+  Expense(
+      {required this.title,
+      required this.amount,
+      required this.date,
+      required this.category})
+      : id = uuid.v4();
+
+  final String id;
+  final String title;
+  final double amount;
+  final DateTime date;
+  final Category category;
+
+  String get formatedDate {
+    return dateFormater.format(date);
+  }
+}
+
+class ExpenseBucket {
+  ExpenseBucket({required this.category, required this.expenses});
+
+  ExpenseBucket.forCategory(List<Expense> allExpenses, this.category)
+      : expenses = allExpenses
+            .where((expense) => expense.category == category)
+            .toList();
+
+  final Category category;
+  final List<Expense> expenses;
+
+  double get totalExpenses {
+    double sum = 0;
+
+    for (final expense in expenses) {
+      sum += expense.amount;
+    }
+
+    return sum;
+  }
+}
